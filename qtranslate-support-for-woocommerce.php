674511998293981801d4d213aa87ef7a0d060429
michel.weimerskirch <michel.weimerskirch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 21:22:39 +0000

    Release 1.0.8
    git-svn-id: https://plugins.svn.wordpress.org/qtranslate-support-for-woocommerce/trunk@1081174 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qtranslate-support-for-woocommerce.php
+++ qtranslate-support-for-woocommerce.php
@@ -3,7 +3,7 @@
 Plugin Name: qTranslate support for WooCommerce
 Plugin URI: https://github.com/mweimerskirch/wordpress-qtranslate-support-for-woocommerce
 Description: Makes qTranslate work with WooCommerce
-Version: 1.0.7
+Version: 1.0.8
 Author: Michel Weimerskirch
 Author URI: http://michel.weimerskirch.net
 License: MIT
@@ -39,6 +39,7 @@ add_filter('the_title_attribute', 'qtrans_useCurrentLanguageIfNotFoundUseDefault
 add_filter('woocommerce_attribute_label', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
 add_filter('woocommerce_variation_option_name', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
 add_filter('woocommerce_page_title', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+add_filter('woocommerce_product_title', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
 add_filter('woocommerce_cart_shipping_method_full_label', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
 
 
